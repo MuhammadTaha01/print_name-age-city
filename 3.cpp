1b0b1d765339d91f2e476526b5f6129a85387f6c
MuhammadTaha01 <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-11 00:50:16 +0500

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name, city;
+    int age;
+    cout<<"Enter your age= ";
+    cin>>age;
+
+    cout<<"Enter your first name= ";
+    cin>>name;
+
+    cout<<"Enter your city= ";
+    cin>>city;
+
+    cout<<"Your first name is "<<name<<endl;
+    cout<<"Your age is "<<age<<endl;
+    cout<<"Your city name is "<<city<<endl;
+    return 0;
+}
